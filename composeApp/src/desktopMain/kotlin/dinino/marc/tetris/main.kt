7e317c46c815e57f221edcc02dcb0ad7df6814ae
Marc DiNino <dinino_marc@bah.com>
2025-05-27 19:04:34 -0400

    Hook up string resource

--- composeApp/src/desktopMain/kotlin/dinino/marc/tetris/main.kt
+++ composeApp/src/desktopMain/kotlin/dinino/marc/tetris/main.kt
@@ -3,12 +3,15 @@ package dinino.marc.tetris
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import dinino.marc.tetris.di.KoinInitializer
+import org.jetbrains.compose.resources.stringResource
+import tetris.composeapp.generated.resources.Res
+import tetris.composeapp.generated.resources.app_name
 
 fun main() = application {
     KoinInitializer.init()
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Tetris",
+        title = stringResource(Res.string.app_name),
     ) {
         App()
     }
